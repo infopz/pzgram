cd5504c24a90def2535ca09af9cee30437c13aca
infopz <casari.giovanni@gmail.com>
2018-03-19 20:18:33 +0100

    0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pzgram',
   packages = ['pzgram'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'A pz-way to create your telegram bot',
   author = 'Giovani Casari',
   author_email = 'casari.giovanni@gmail.com',
