b84b32effc7203d9307f3d9111f598fe203cf308
Giovanni Casari <casari.giovanni@gmail.com>
2018-03-19 19:26:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
 setup(
   name = 'pzgram',
-  packages = ['pzgram'], # this must be the same as the name above
+  packages = ['pzgram'],
   version = '0.2',
   description = 'A pz-way to create your telegram bot',
   author = 'Giovani Casari',
   author_email = 'casari.giovanni@gmail.com',
-  url = 'https://github.com/infopz/pzgram', # use the URL to the github repo
-  download_url = 'https://github.com/infopz/pzgram/archive/0.2.tar.gz', # I'll explain this in a second
-  keywords = ['telegram', 'telegram-bot', 'telegram-api'], # arbitrary keywords
+  url = 'https://github.com/infopz/pzgram',
+  download_url = 'https://github.com/infopz/pzgram/archive/0.2.tar.gz',
+  license = 'LICENSE.txt',
+  install_requires = ["requests >= 2.18.4"],
+  keywords = ['telegram', 'telegram-bot', 'telegram-api'],
   classifiers = [],
 )
