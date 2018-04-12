2d4561a1179b502a554779bec309c70ce6541bea
Giovanni Casari <casari.giovanni@gmail.com>
2018-04-12 14:02:40 +0200

    Update inlinePoolBot.py

--- Examples/inlinePoolBot.py
+++ Examples/inlinePoolBot.py
@@ -4,6 +4,7 @@ bot = pzgram.Bot("BOTKEY")
 pool_message = ""
 
 # Create Pool Keyboard
+# Button parametes: first the text of the button, second the data of that button (see explanation)
 button1 = pzgram.create_button("1", "choice1")
 button2 = pzgram.create_button("2", "choice2")
 k = [[button1, button2]] # Format button like "normal" keyboards
@@ -38,3 +39,12 @@ queries = {"choice1": catch_results, "choice2": catch_results}
 bot.set_query(queries)
 
 bot.run()
+
+# EXPLANATION (Italian)
+# Tramite il comando /pool si richiama la funzione pool_command, che invia il messaggio di Pool
+# utilizzando la tastiera creada nelle prime righe
+# Quando l'utente spinge uno dei due pulsanti, al bot arriva un "messaggio" (query) contenente l'utente che ha spinto
+# il pulsante, il messaggio originale, e un campo "data" che serve per differenziare le varie query, come i nomi
+# dei comandi (per esempio)
+# Tramite bot.set_query vado a dirgli quali funzioni richiamare quando arriva una query con un determinato campo "data",
+# in questo caso, choice1 e choice2 che vanno entrambi a richiamre la stessa funzione
